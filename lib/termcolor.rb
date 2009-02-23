b10a87b032ed3570f3d6048f4183373462b75285
jugyo <jugyo.org@gmail.com>
2009-02-24 00:31:33 +0900

    implement

--- lib/termcolor.rb
+++ lib/termcolor.rb
@@ -1,15 +1,55 @@
-require "rexml/document"
+require 'highline'
+require 'cgi'
+require 'rexml/parsers/streamparser' 
+require 'rexml/parsers/baseparser' 
+require 'rexml/streamlistener' 
 
 class TermColor
   VERSION = '0.0.1'
+  include REXML
+
+  class ParseError < StandardError; end
 
   class << self
     @highline = HighLine.new
     def print(text)
       print parse(text)
     end
+
     def parse(text)
-      # TODO
+      listener = MyListener.new 
+      REXML::Parsers::StreamParser.new(text, listener).parse
+      listener.result
+    rescue REXML::ParseException => e
+      raise ParseError, e
     end
+  end
+
+  class MyListener 
+    include REXML::StreamListener 
+
+    attr_reader :result
+    def initialize
+      @result = ''
+      @tag_stack = []
+    end
+
+    def tag_start(name, attrs)
+      esc_seq = HighLine.const_get(name.upcase)
+      @result << esc_seq
+      @tag_stack.push(name)
+    rescue NameError
+    end
+
+    def text(text)
+      @result << CGI.unescapeHTML(text)
+    end
+
+    def tag_end(name)
+      @tag_stack.pop
+      @result << HighLine::CLEAR
+      @result << HighLine.const_get(@tag_stack[-1].upcase) unless @tag_stack.empty?
+    end
+
   end
 end
--- spec/termcolor_spec.rb
+++ spec/termcolor_spec.rb
@@ -5,7 +5,29 @@ require File.dirname(__FILE__) + '/spec_helper'
 class TermColor
   describe TermColor do
     before do
-      @termcolor = TermColor.new
+    end
+
+    it 'should parse 1' do
+      text = TermColor.parse('aaa<red>aaaa<bold>foo</bold>bb<blue>bbbb</blue>bbb</red>ccc<on_yellow>ccccc</on_yellow>ccc')
+      puts text
+      text.should == "aaa\e[31maaaa\e[1mfoo\e[0m\e[31mbb\e[34mbbbb\e[0m\e[31mbbb\e[0mccc\e[43mccccc\e[0mccc"
+    end
+
+    it 'should parse 2' do
+      text = TermColor.parse('aa<blue>a<foo>aaa<red>aa</red>aaaa</foo>a</blue>aaa')
+      puts text
+      text.should == "aa\e[34maaaa\e[31maa\e[0m\e[34maaaa\e[0ma\e[0maaa"
+    end
+
+    it 'should parse 3' do
+      text = TermColor.parse('aa<blue>aaaaa&lt;aaa&quot;aaa&gt;aaa&amp;aaaaa</blue>aaa')
+      puts text
+      text.should == "aa\e[34maaaaa<aaa\"aaa>aaa&aaaaa\e[0maaa"
+    end
+
+    it 'should raise Error' do
+      lambda{ TermColor.parse('aaaaa<red>aaaaa</blue>aaaaa') }.should raise_error(TermColor::ParseError)
+      lambda{ TermColor.parse('aaaaa<red>aaaaaaaaaa') }.should_not raise_error(TermColor::ParseError)
     end
   end
 end
