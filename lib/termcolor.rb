01525528d39f66563d5d914c77849fa5dc368d6c
jugyo <jugyo.org@gmail.com>
2009-02-23 23:08:57 +0900

    init

---
+++ lib/termcolor.rb
@@ -0,0 +1,5 @@
+require 'termcolor/connection'
+
+class TermColor
+  VERSION = '0.0.1'
+end
---
+++ spec/termcolor_spec.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+require File.dirname(__FILE__) + '/spec_helper'
+
+class TermColor
+  describe TermColor do
+    before do
+      @termcolor = TermColor.new
+    end
+  end
+end
